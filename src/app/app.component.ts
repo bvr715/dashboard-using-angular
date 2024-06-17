ac3baa6b9b791bb2235414e3fb71aea7f80b2122
bvr715 <venkateswararao21021@gmail.com>
2024-06-17 16:29:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+
+  ngOnInit(): void {
+
+  }
 }
